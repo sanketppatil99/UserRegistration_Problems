4fe6181d72070286b62b0366da6c98a2f82071dc
FIRST_NAME LAST_NAMEsanketppatil99 <sanketppatil99@gmail.com>
2023-03-03 22:44:09 +0530

    Check first name is valid or not

---
+++ DetailCheck.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationProblem
+{
+    public class DetailCheck
+    {
+        public static string reg = "^[A-Z][A-za-z]{2,}";
+
+        public static bool Validate(String word)
+        {
+            return Regex.IsMatch(word, reg);
+        }
+    }
+}
