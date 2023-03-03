146e7f92ac59aabebe7ee1a4ff5a6af6fff3d058
FIRST_NAME LAST_NAMEsanketppatil99 <sanketppatil99@gmail.com>
2023-03-03 22:43:30 +0530

    Check first name is valid or not

--- Program.cs
+++ Program.cs
@@ -5,6 +5,17 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to User Registration Problem");
+            Console.WriteLine("Enter the first name");
+            String FName = Console.ReadLine();
+
+            if (DetailCheck.Validate(FName))
+            {
+                Console.WriteLine("Valid Name");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Name");
+            }
         }
     }
 }
