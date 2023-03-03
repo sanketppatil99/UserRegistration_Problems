48b01affb51dd2c56225eab328cb3659e046408e
FIRST_NAME LAST_NAMEsanketppatil99 <sanketppatil99@gmail.com>
2023-03-03 22:33:18 +0530

    Greeting message

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace UserRegistrationProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registration Problem");
+        }
+    }
+}
